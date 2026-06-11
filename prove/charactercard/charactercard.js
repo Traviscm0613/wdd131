fb121bedc67e597f38cf3266a5fca16d75c133a1
Maxwell0613-user <traviscm0613@gmail.com>
2026-06-11 16:28:40 -0600

    charactercard assignment

--- prove/charactercard/charactercard.js
+++ prove/charactercard/charactercard.js
@@ -1,40 +1,59 @@
 const character = {
-  sections: [
-    {
-      level: 5,
-      health: 100
+  level: 5,
+  health: 100,
+
+  attack: function () {
+    this.health = this.health - 10;
+
+    if (this.health <= -10) {
+    this.health = 0;
+
+    alert("Character Died");
+    return;
     }
-  ]
+
+    renderCharacter();
+  },
+
+  levelUp: function () {
+    this.level += 1;
+    renderCharacter();
+  }
 };
 
-function sectionTemplate(sections) {
-  return `
-    <tr>
-      <td>${sections.level}</td>
-      <td>${sections.health}</td>
-    </tr>
-  `;
+function renderCharacter() {
+  document.querySelector("#level").textContent = character.level;
+  document.querySelector("#health").textContent = character.health;
 }
 
-function renderSections(sections) {
-  document.querySelector("#sections").innerHTML =
-    sections.map(sectionTemplate).join("");
-}
+renderCharacter();
 
-renderSections(character.sections);
+document.querySelector("#attack").addEventListener("click", function () {
+  character.attack();
+});
 
-const character = {
-    
+document.querySelector("#levelUp").addEventListener("click", function () {
+  character.levelUp();
+});
+
+///////////////
+
+
+/*
+for refrence from courses ponder
+
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js.png',
     sections: [
-    {
-      level: 5,
-      health: 100
-    }
-  ],
-    attack: function (level) {
+    { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+    ],
+    enrollStudent: function (sectionNum) {
         // find the right section...Array.findIndex will work here
         const sectionIndex = this.sections.findIndex(
-          (section) => section.level == level
+          (section) => section.sectionNum == sectionNum
         );
         if (sectionIndex >= 0) {
           this.sections[sectionIndex].enrolled++;
@@ -43,10 +62,17 @@ const character = {
       }
   };
            
+document.querySelector('#courseName').textContent = aCourse.name;
+document.querySelector('#courseName').textContent = aCourse.code;
+
+
 function sectionTemplate(section) {
     return `<tr>
-      <td>${section.level}</td>
-      <td>${section.health}</td>`
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
 }
 
 function renderSections(sections) {
@@ -57,7 +83,7 @@ document.querySelector("#sections").innerHTML = html.join("");
 renderSections(aCourse.sections);
 
 document.querySelector("#enrollStudent").addEventListener("click", function () {
-    const level = document.querySelector("#level").value;
-    aCourse.enrollStudent(level);
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
 });
-        
+*/
