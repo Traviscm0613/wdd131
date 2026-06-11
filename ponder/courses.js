1740aaae8ad8279d83f1a445dd225fe4d0b5b1bc
Maxwell0613-user <traviscm0613@gmail.com>
2026-06-10 21:16:47 -0600

    created html for charactercard

--- ponder/courses.js
+++ ponder/courses.js
@@ -1,7 +1,7 @@
 const aCourse = {
     code: 'CSE121b',
     name: 'Javascript Language',
-    logo: 'images/js-logo.png',
+    logo: 'images/js.png',
     sections: [
     { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
     { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
@@ -26,14 +26,18 @@ aCourse.code = 'WDD131';
 
 console.log(aCourse.code);
 
-document.querySelector('#courseName').textContent = aCourse.name;
-document.querySelector('#courseName').textContent = aCourse.code;
 
 console.log(aCourse.sections[0].roomNum)
 
 aCourse.enrollStudent();
 */
+document.querySelector('#courseName').textContent = aCourse.name;
+document.querySelector('#courseName').textContent = aCourse.code;
 
+
+document.querySelector("img").setAttribute("src", aCourse.logo);
+document.querySelector("img").setAttribute("alt", aCourse.logo);
+document.querySelector("img").style.width = '100px'
 
 function sectionTemplate(section) {
     return `<tr>
